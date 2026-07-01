745d9fa9ab73a559a9636b63c26c07ec77582f64
Sonali Rai <sonalirai908@gmail.com>
2026-07-01 22:10:46 +0530

    added new program

---
+++ chapter 7/break_continue_pass.py
@@ -0,0 +1,17 @@
+#break statement
+for i in range(5):
+    if(i==3):
+        break # exit the loop right now
+    print(i)
+
+#continue statement
+for j in range(5):
+    if (j==3):
+        continue #only skip that value
+    print(j)
+
+#pass statement
+for k in range(5):
+    if(k==3):
+        pass # just pass 
+    print(k)
--- chapter 7/loops.py
+++ chapter 7/loops.py
@@ -1,6 +1,5 @@
 #loops inpython are used to repeat a block of code multiple times
-#for loop - used when you know how many times you want to repeat
-            #or when you want to iterate over a sequence
+
 
 # printing numbers 1 to 5
 for i in range(1,6):
