6368aa841b2a3893f95eb7bcb8d74ea7809eea9c
Sonali Rai <sonalirai908@gmail.com>
2026-07-01 21:32:50 +0530

    added new program

---
+++ chapter 7/loops.py
@@ -0,0 +1,12 @@
+#loops inpython are used to repeat a block of code multiple times
+#for loop - used when you know how many times you want to repeat
+            #or when you want to iterate over a sequence
+
+# printing numbers 1 to 5
+for i in range(1,6):
+    print(i)            
+
+#printing each character of astring
+name="Sonali"
+for ch in name:
+    print(ch)
